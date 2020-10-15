fc07fc50e5639fcc7137c7d5189a8692a38e52c9
saurabh <saurabhchavan134@gmail.com>
2020-10-15 12:28:53 +0530

    generated unique names from docker containers

---
+++ generate_docker_names/v1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# WARNING
+# Line 12 erases current docker containers and 
+# generates new ones.
+
+containersToGenereate=1000
+writeToFile='docker_names.txt'
+
+touch $writeToFile
+
+sudo docker rm $(sudo docker ps -aq)
+
+i=0
+while [ $i -lt $containersToGenereate ]
+do
+    echo $i
+    sudo docker run -d hello-world
+    i=`expr $i + 1`
+done
+
+sleep 1
+
+sudo docker ps -a | awk '{print $12}' > $writeToFile
+cat $writeToFile
+#sudo docker ps -a
